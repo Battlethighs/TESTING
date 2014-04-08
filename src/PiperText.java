f08fde032c01969711eb407f062be02c7d318ee3
Andrew <ahansen4@macalester.edu>
2014-04-07 22:54:19 -0500

    Initial commit

---
+++ src/PiperText.java
@@ -0,0 +1,29 @@
+/**
+ * Created by Andrew on 4/7/14.
+ */
+public class PiperText {
+
+    private String title = null;
+    private String body = null;
+
+
+    public void setTitle (String input){
+        this.title = input;
+    }
+    public void setBody (String input){
+        this.body = input;
+    }
+    public String getTitle (){
+        return title;
+    }
+    public String getBody (){
+        return body;
+    }
+
+    public PiperText () {
+
+
+    }
+
+
+}
